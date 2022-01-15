1086ed174ceb18d56fe7c831f2f14952b9f46aa4
Abdul Samad <sam_here@outlook.com>
2022-01-15 23:30:06 +0500

    added all seo descriptions. tyring them out now

--- src/app/view/landing/landing.component.ts
+++ src/app/view/landing/landing.component.ts
@@ -56,14 +56,29 @@ export class LandingComponent implements OnInit {
             name: 'og:image', content: 'https://asknamer.com/assets/images/meta.png'
         });
 
-
-
         // <!-- Twitter -->
         // <meta property="twitter:card" content="summary_large_image">
         // <meta property="twitter:url" content="https://metatags.io/">
         // <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
         // <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
         // <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+        // <!-- Twitter -->
+        this.metaTagService.updateTag({
+            name: 'twitter:card', content: 'summary_large_image'
+        });
+        this.metaTagService.updateTag({
+            name: 'twitter:url', content: 'https://asknamer.com/'
+        });
+        this.metaTagService.updateTag({
+            name: 'twitter:title', content: 'Ask Namer | Naming Agency | Haecceity'
+        });
+        this.metaTagService.updateTag({
+            name: 'twitter:description', content: 'We make your brand, startup, product name up to 7 times easier to remember than a normal name, providing it a lifelong advantage.'
+        });
+        this.metaTagService.updateTag({
+            name: 'twitter:image', content: 'https://asknamer.com/assets/images/meta.png'
+        });
 
 
 
