2b0961ece64234b17d94675922d86a202d70d15a
Abdul Samad <sam_here@outlook.com>
2022-01-16 01:45:53 +0500

    updated sitemap timestamps

--- src/app/view/landing/landing.component.ts
+++ src/app/view/landing/landing.component.ts
@@ -33,12 +33,6 @@ export class LandingComponent implements OnInit {
             name: 'keywords', content: 'brand name, startup name, business name, naming agency, branding'
         });
 
-        // <meta property="og:type" content="website">
-        // <meta property="og:url" content="https://metatags.io/">
-        // <meta property="og:title" content="Meta Tags — Preview, Edit and Generate">
-        // <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-        // <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
-
         // <!-- Open Graph / Facebook -->
         this.metaTagService.updateTag({
             name: 'og:type', content: 'website'
@@ -57,13 +51,6 @@ export class LandingComponent implements OnInit {
         });
 
         // <!-- Twitter -->
-        // <meta property="twitter:card" content="summary_large_image">
-        // <meta property="twitter:url" content="https://metatags.io/">
-        // <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate">
-        // <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!">
-        // <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
-
-        // <!-- Twitter -->
         this.metaTagService.updateTag({
             name: 'twitter:card', content: 'summary_large_image'
         });
@@ -79,8 +66,6 @@ export class LandingComponent implements OnInit {
         this.metaTagService.updateTag({
             name: 'twitter:image', content: 'https://asknamer.com/assets/images/meta.png'
         });
-
-
 
         gsap.registerPlugin(ScrollToPlugin);
         gsap.registerPlugin(ScrollTrigger);
