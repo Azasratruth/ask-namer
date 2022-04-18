71e56e4ad1f77d46cd546bd7aa731e6ce99db4a4
Abdul Samad <sam_here@outlook.com>
2022-04-18 10:46:39 +0500

    fixed footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,37 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import gsap from 'gsap';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.css']
 })
 
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  public linkHoverOn(object) {
-    gsap.to(object, {
-      paddingTop: 8,
-      paddingBottom: 8,
-      paddingLeft: 35,
-      paddingRight: 35,
-      backgroundColor: '#fff',
-      color: '#000',
-      duration: 0.3
-    });
-  }
+	public linkHoverOn(object, text) {
+		gsap.to(object, {
+			paddingTop: 8,
+			paddingBottom: 8,
+			paddingLeft: 35,
+			paddingRight: 35,
+			backgroundColor: '#fff',
+			duration: 0.3
+		});
 
-  public linkHoverOff(object) {
-    gsap.to(object, {
-      padding: 0,
-      color: '#fff',
-      backgroundColor: 'inherit',
-      duration: 0.3
-    });
-  }
+		gsap.to(text, {
+			color: '#000',
+			duration: 0.3
+		});
+	}
+
+	public linkHoverOff(object, text) {
+		gsap.to(object, {
+			padding: 0,
+			backgroundColor: 'inherit',
+			duration: 0.3
+		});
+
+		gsap.to(text, {
+			color: '#fff',
+			duration: 0.3
+		});
+	}
 
 }
