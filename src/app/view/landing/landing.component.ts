bfef3c422342ac44162cd657dfa3efa84b12abfa
Abdul Samad <sam_here@outlook.com>
2022-04-25 06:30:23 +0500

    based to oriented

--- src/app/view/landing/landing.component.ts
+++ src/app/view/landing/landing.component.ts
@@ -5,31 +5,31 @@ import { gsap } from 'gsap';
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+	selector: 'app-landing',
+	templateUrl: './landing.component.html',
+	styleUrls: ['./landing.component.css']
 })
 
 export class LandingComponent implements OnInit {
 
-  constructor(
-    private titleService: Title,
-    private metaTagService: Meta,
-  ) {
-    this.titleService.setTitle('Ask Namer | Leading Research-Based Naming Agency');
-    this.metaTagService.addTags([
-      { name: 'description', content: 'At Ask Namer, we create memorable, evocative, and engaging names for companies, brands, and products.' },
-      { name: 'keywords', content: 'brand name, startup name, business name, naming agency, branding' },
-    ]);
-  }
+	constructor(
+		private titleService: Title,
+		private metaTagService: Meta,
+	) {
+		this.titleService.setTitle('Ask Namer | Leading Research-Oriented Naming Agency');
+		this.metaTagService.addTags([
+			{ name: 'description', content: 'At Ask Namer, we create memorable, evocative, and engaging names for companies, brands, and products.' },
+			{ name: 'keywords', content: 'brand name, startup name, business name, naming agency, branding' },
+		]);
+	}
 
-  ngOnInit(): void {
-    gsap.registerPlugin(ScrollToPlugin);
+	ngOnInit(): void {
+		gsap.registerPlugin(ScrollToPlugin);
 
-    this.openingAnimation();
-  }
+		this.openingAnimation();
+	}
 
-  private openingAnimation() {
-    gsap.to(window, { duration: 0, scrollTo: 0 });
-  }
+	private openingAnimation() {
+		gsap.to(window, { duration: 0, scrollTo: 0 });
+	}
 }
