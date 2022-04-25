290e3f54a27b64877f356ce1b8716f2c951407c1
Abdul Samad <sam_here@outlook.com>
2022-04-25 06:09:23 +0500

    added a picture in place of the forward and backwards symbol

--- src/app/view/back-button-for-blogs/back-button-for-blogs.component.ts
+++ src/app/view/back-button-for-blogs/back-button-for-blogs.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-back-button-for-blogs',
-  templateUrl: './back-button-for-blogs.component.html',
-  styleUrls: ['./back-button-for-blogs.component.css']
+	selector: 'app-back-button-for-blogs',
+	templateUrl: './back-button-for-blogs.component.html',
+	styleUrls: ['./back-button-for-blogs.component.css', '../back-button-for-names/back-button-for-names.component.css']
 })
 export class BackButtonForBlogsComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
